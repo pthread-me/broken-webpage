43564bef86da5fa62bd7a8da7880932f09553fb4
pthread-me <anas.n.alhadi@outlook.com>
2025-06-09 11:22:54 -0300

    Finally fixed theme switching

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,4 @@
-// Man fuck JS
-// if only you knew the amount of time it took to debug this shit
-// give me types any mfing day of the week :(
+// Theme and transition controllers
 
 function getTheme() {
   if (typeof localStorage !== 'undefined' && localStorage.getItem('theme') !==null ) {
@@ -12,23 +10,12 @@ function getTheme() {
   return 0;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("dom loaded")
-	const theme = getTheme();
-	console.log("theme found was " + theme)
-	if (theme == '0')	{
-		document.getElementsByTagName('html')[0].classList.toggle('dark');
-		localStorage.setItem('theme', '0')
-	}else{
-		localStorage.setItem('theme', '1')
-	}
-});
-
-
 const toggleButton = document.getElementById('theme_change'); 
 if (toggleButton) {
   toggleButton.addEventListener('click', () => {
+		document.getElementsByTagName('html')[0].classList.add('theme-transition');
 		document.getElementsByTagName('html')[0].classList.toggle('dark')
 		localStorage.setItem('theme', getTheme()^1);
   });
 }
+
